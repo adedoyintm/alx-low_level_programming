b81f90c4a57957d5189b8e37514bd8e789f062e0
Adedoyin Adegboyega <adegboyegadoyin@gmail.com>
2022-09-08 11:24:52 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,12 @@
 #!/bin/bash
-
-
-#include <stdio.h>
-
-/**
- *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
-
-int main(void)
-
-{
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
-}
-
-
+1 #include <stdio.h>
+2 /**
+    3 * main - main block
+    4 *
+    5 *Return: 0 after printing the function
+    6 */
+7 int main(void)
+	8 {
+		9 puts("\"Programming is like building a multilingual puzzle");
+		10 return (0);
+		11 }
