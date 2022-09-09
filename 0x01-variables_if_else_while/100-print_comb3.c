a7cb43de14d14ebc65aa0c36e4095c790abd73e5
Adedoyin Adegboyega <adegboyegadoyin@gmail.com>
2022-09-09 13:56:15 -0700

    inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,10 +2,11 @@
 
 /**
  * main -> assign a random number to the variable n each time it is executed
- * and print the last digit of the number stored in the variable n
+ * 
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
 	int ch;
 	int n;
