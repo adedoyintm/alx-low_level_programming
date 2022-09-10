7fd3478ef223e65c8e4b8855f48bd5ec88624ef6
Adedoyin Adegboyega <adegboyegadoyin@gmail.com>
2022-09-10 11:33:22 -0700

    combination of brains

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success/completed)
- */
 
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
 int main(void)
 {
-	int d;
-
-	for (d = 0; d < 100; d++)
+	int n, m;
+	/**/
+	for (n = 48; n <= 56; n++)
 	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 99)
+		for (m = 49; m <= 57; m++)
 		{
-			putchar(',');
-			putchar(' ');
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
-	
