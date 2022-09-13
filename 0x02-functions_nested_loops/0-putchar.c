8046fedea5725019f924e116b130109f142a3b29
Adedoyin Adegboyega <adegboyegadoyin@gmail.com>
2022-09-13 08:38:11 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,21 +4,20 @@
 /**
  * main - Entry point
  *
- * Description: prints _putchar
- *
  * Return: nothing, return void
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
