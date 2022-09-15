43dd5a5e7013438385282e968ffc0ba9203ff0da
Adedoyin Adegboyega <adegboyegadoyin@gmail.com>
2022-09-14 20:28:30 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,46 +1,46 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * times_table - prints times table
- * Return:void
+ * times_table -prints tables
+ * Return: Always 0.
  */
-
 void times_table(void)
 {
-
-	int a = 0;
+	int a;
 	int b;
-	int rep;
-	while (a <= 9)
+	int c;
+	for (a = 0; a <= 9; a++)
 	{
-		b = 0;
-		while (b <= 9)
+		for (b = 0; b <= 9; b++)
 		{
-			rep = a * b;
-			if (b == 0)
+			c = a * b;
+			if ((c / 10) == 0)
 			{
-				_putchar('0' + rep);
-			}
-			else if (rep < 10)
-			{
-				_putchar(' ');
-				_putchar('0' + rep);
+				if (b == 0)
+				{
+					_putchar ('0');
+				}
+				if (b != 0)
+				{
+					_putchar (' ');
+					_putchar ((c % 10) + '0');
+				}
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
 			}
 			else
 			{
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
+				_putchar ((c / 10) + '0');
+				_putchar ((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
 			}
-
-			if (b < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			b++;
 		}
-		_putchar('\n');
-		a++;
+		_putchar ('\n');
 	}
-
 }
